600240d27f501ad0bec8965ca6cba90db7e52817
Meeple <jocult001@gmail.com>
2021-10-13 00:53:10 +0100

    init

--- build.sh
+++ build.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 git commit -a -m init
-git clean -dfx
 gbp buildpackage --git-ignore-new --git-export-dir=../cluster-artifacts --git-debian-branch=master --git-upstream-branch=master --git-upstream-tree=master 
+git clean -dfx
+cp ../cluster-artifacts/clusterctrl_1.0-1.0_arm64.deb /mnt/win/MNT/home/pi
 
