ce42521c4e315d81d66c386b6332583b9f9955e0
Meeple <jocult001@gmail.com>
2021-10-13 10:59:58 +0100

    init

--- build.sh
+++ build.sh
@@ -4,5 +4,5 @@ rm -r /mnt/win/cluster-artifacts/*
 gbp buildpackage --git-ignore-new --git-export-dir=../cluster-artifacts --git-debian-branch=master --git-upstream-branch=master --git-upstream-tree=master
 git clean -dfx
 sudo chroot /mnt/win/MNT /usr/bin/apt remove clusterctrl -y
-cp /mnt/win/cluster-artifacts/clusterctrl_1.0-1.0_arm64.deb /mnt/win/MNT/home/pi/clusterctrl.deb
-sudo chroot /mnt/win/MNT apt install /home/pi/clusterctrl.deb
+cp /mnt/win/cluster-artifacts/clusterctrl_1.0-1.0_arm64.deb /mnt/win/MNT2/home/pi/clusterctrl.deb
+sudo chroot /mnt/win/MNT2 apt install /home/pi/clusterctrl.deb
