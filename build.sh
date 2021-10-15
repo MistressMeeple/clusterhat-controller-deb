a256944d847dfb44601889d4472c9130a3c19ce2
meeplex <jocult001@gmail.com>
2021-10-16 00:42:25 +0100

    init

--- build.sh
+++ build.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 git commit -a -m init
 #rm $(ls -I ".deb" /mnt/win/cluster-artifacts/)
+rm /mnt/win/cluster-artifacts/clusterctrl*.orig.tar.gz
 gbp buildpackage --git-ignore-new --git-export-dir=../cluster-artifacts --git-debian-branch=master --git-upstream-branch=master --git-upstream-tree=master
 git clean -dfx
 #sudo chroot /mnt/win/MNT /usr/bin/apt remove clusterctrl -y
